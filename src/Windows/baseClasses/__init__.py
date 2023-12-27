c43e445aa87fb0ff6a8e25c35b33252d1ac59da2
JonScha <jonas123674@gmail.com>
2023-12-27 22:04:29 +0100

    fixed import Error (new Plugins.baseClasses directory)

--- src/Windows/baseClasses/__init__.py
+++ src/Windows/baseClasses/__init__.py
@@ -1,5 +1,4 @@
 from .baseFunctionWindow import BaseFunctionWindow
 from .resultWindow import ResultWindow
 from .BaseColumnWindow import BaseColumnWindow
-#from .BaseSimplePlugin import BaseSimplePlugin
-from .basePlugin import BasePlugin
+from Plugins.baseClasses.basePlugin import BasePlugin
